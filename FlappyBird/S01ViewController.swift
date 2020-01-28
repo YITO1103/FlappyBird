f611e5f580b7d66b43c5d97fdf00b10f2c20bcac
YITO1103 <yuta.j.1103@gmail.com>
2020-01-28 19:25:34 +0900

    012803 うまくいってない

--- FlappyBird/S01ViewController.swift
+++ FlappyBird/S01ViewController.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import YLGIFImage
+
 
 class S01ViewController: UIViewController {
 
+    @IBOutlet weak var demoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        loadGifAnimation() 
         sleep(10)
         self.dismiss(animated: true, completion: nil)
 
@@ -22,5 +26,12 @@ class S01ViewController: UIViewController {
         self.present(viewController!, animated: true, completion: nil)
         //self.dismiss(animated: true, completion: nil)
     }
+    func loadGifAnimation() {
 
-}
+        if let path = Bundle.main.url(forResource: "1111", withExtension: "gif") {
+            if let data = try? Data(contentsOf: path) {
+                let gifImage = YLGIFImage(data: data)
+                self.demoImageView.image = gifImage
+            }
+        }
+    }}
